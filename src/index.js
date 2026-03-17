9b1a63bfcdef2b7181831686ab6b39b30aa154c8
pashathelooser <pavelvatutin08@gmail.com>
2026-03-17 19:01:31 +0500

    task 5 done

--- src/index.js
+++ src/index.js
@@ -20,11 +20,40 @@ export class Duck extends Creature {
     }
 }
 
+export class Gatling extends Creature {
+    constructor(name = 'Гатлинг', power = 6) {
+        super(name, power);
+    }
+
+    attack(gameContext, continuation){
+        const taskQueue = new TaskQueue();
+        const { oppositePlayer, position } = gameContext;
+        taskQueue.push(onDone => this.view.showAttack(onDone));
+
+        for (let i = 0; i < oppositePlayer.table.length; i++) {
+            const targetCard = oppositePlayer.table[i];
+            
+            if (!targetCard) continue;
+
+            taskQueue.push(onDone => {
+                this.dealDamageToCreature(2, targetCard, gameContext, onDone);
+            });
+        }
+
+        taskQueue.continueWith(continuation);
+    }
+    
+    dealDamageToPlayer(value, gameContext, continuation) {
+        continuation();
+    }
+}
+
 // Класс Собака
 export class Dog extends Creature {
     constructor(name = 'Пес-бандит', power = 3) {
         super(name, power);
     }
+
 }
 
 //Класс Громила 
@@ -57,7 +86,7 @@ function isDog(card) {
     return card instanceof Dog;
 }
 
-function getCreatureDescription(card) {
+export function getCreatureDescription(card) {
     if (isDuck(card) && isDog(card)) {
         return 'Утка-Собака';
     }
@@ -70,16 +99,16 @@ function getCreatureDescription(card) {
     return 'Существо';
 }
 
-// 🃏 Колоды для проверки 
 const seriffStartDeck = [
     new Duck(),
     new Duck(),
     new Duck(),
-    new Duck(),
+    new Gatling(),
 ];
-
 const banditStartDeck = [
-    new Trasher(),  
+    new Trasher(),
+    new Dog(),
+    new Dog(),
 ];
 
 // Создание игры.
